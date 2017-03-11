8da8321ae371758b6eb1d5b7333e69c35bd362af
Ulrik Lejon <ulrik.lejon@mollyware.se>
2017-03-11 20:36:21 +0100

    Changes

---
+++ computerspecific/home-linux/android.zsh
@@ -0,0 +1,5 @@
+# The next line updates PATH for the Google Cloud SDK.
+source '/home/ul/tools/versions/google-cloud-sdk/path.zsh.inc'
+
+# The next line enables shell command completion for gcloud.
+source '/home/ul/tools/versions/google-cloud-sdk/completion.zsh.inc'
--- envloader.zsh
+++ envloader.zsh
@@ -1,12 +1,12 @@
 function seLejon_loadCommonn {
-	for file in $ZSH_CUSTOM/common/*.zsh; do 
-		source $file; 
+	for file in $ZSH_CUSTOM/common/*.zsh; do
+		source $file;
 	done
 }
 
-function seLejon_loadOsSpecific { 
+function seLejon_loadOsSpecific {
 	value=$(uname)
-	for file in $ZSH_CUSTOM/osspecific/$value/*.zsh; do 
+	for file in $ZSH_CUSTOM/osspecific/$value/*.zsh; do
 		source $file;
 	done
 }
@@ -14,11 +14,10 @@ function seLejon_loadOsSpecific {
 function seLejon_loadComputerSpecific {
 	value=$(<~/.computer_id)
 	if [ -d "$ZSH_CUSTOM/computerspecific/$value" ]; then
-    	for file in $ZSH_CUSTOM/computerspecific/$value/*.zsh; do 
-			echo $file;
+    	for file in $ZSH_CUSTOM/computerspecific/$value/*.zsh; do
 			source $file; 
-		done	
-	fi	
+		done
+	fi
 }
 
 seLejon_loadCommonn
