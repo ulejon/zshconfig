0cd3dfa1aebaf9d72e2dc26b2f03a9df5f48cd72
Ulrik Lejon <ulrik.lejon@mollyware.se>
2017-03-11 20:41:34 +0100

    Added scala

--- osspecific/Darwin/java.zsh
+++ osspecific/Darwin/java.zsh
@@ -2,3 +2,6 @@ export JAVA_HOME8=/Library/Java/JavaVirtualMachines/jdk1.8.0_121.jdk/Contents/Ho
 export JAVA_HOME=${JAVA_HOME8}
 export JAVA_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2048M"
 export PATH=$PATH:${JAVA_HOME}/bin
+
+export SCALA_HOME=~/tools/scala
+export PATH=$PATH:${SCALA_HOME}/bin
