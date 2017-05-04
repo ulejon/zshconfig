9d2c24869c5ff88249c53336386bfec64f9dfa6f
Ulrik Lejon <ulrik.lejon@mollyware.se>
2017-05-04 12:55:38 +0200

    Added run commands for docker stuff

--- computerspecific/mollyware-osx/sifo.zsh
+++ computerspecific/mollyware-osx/sifo.zsh
@@ -4,3 +4,11 @@ function prod() {
   docker tag docker.tns-sifo.se/$1:${TAG} docker.tns-sifo.se/$1:prod
   docker push docker.tns-sifo.se/$1:prod
 }
+
+function runKeycloakDocker() {
+  docker run --name keycloak -d -p 8080:8080 -e POSTGRES_PORT_5432_TCP_ADDR=172.17.0.2 -e POSTGRES_PORT_5432_TCP_PORT=5432 -e POSTGRES_PASSWORD=postgres docker.tns-sifo.se/keycloak
+}
+
+function runPostgresDocker() {
+  docker run --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=postgres -d docker.tns-sifo.se/docker-postgres-development
+}
