c03fc4e9275d515707de250aec377a6a95ea9d07
Ulrik Lejon <ulrik.lejon@gmail.com>
2018-11-12 14:54:01 +0100

    New java version

--- osspecific/Darwin/java.zsh
+++ osspecific/Darwin/java.zsh
@@ -1,4 +1,4 @@
-export JAVA_HOME8=/Library/Java/JavaVirtualMachines/jdk1.8.0_161.jdk/Contents/Home
+export JAVA_HOME8=/Library/Java/JavaVirtualMachines/jdk1.8.0_192.jdk/Contents/Home
 export JAVA_HOME=${JAVA_HOME8}
 export JAVA_OPTS="-Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2048M"
 export PATH=$PATH:${JAVA_HOME}/bin
