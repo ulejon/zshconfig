ba0c65d716eb66574715682ee0b809449de3b746
Ulrik Lejon <ulrik.lejon@gmail.com>
2020-06-15 17:48:33 +0200

    Added personal sqs queue for orders

--- computerspecific/mollyware-osx/sifo.zsh
+++ computerspecific/mollyware-osx/sifo.zsh
@@ -1,7 +1,7 @@
 export SAMBERA_STORAGE="=$HOME/Documents/sifo/field-report-storage"
 export SAMBERA_PANEL_SQS_PANELLIST_ACTIVITY=https://sqs.eu-west-1.amazonaws.com/569128858329/sqs-panellist-activity-dev-lejonu
 export SAMBERA_PANEL_SQS_ORDER_NEW_ORDER=https://sqs.eu-west-1.amazonaws.com/569128858329/sqs-order-new-order-dev-lejonu
-export SAMBERA_PANEL_SQS_ORDER_CHARGE_ORDER=https://sqs.eu-west-1.amazonaws.com/569128858329/sqs-order-charge-order-dev-dev-lejonu
+export SAMBERA_PANEL_SQS_ORDER_CHARGE_ORDER=https://sqs.eu-west-1.amazonaws.com/569128858329/sqs-order-charge-order-dev-lejonu
 export SAMBERA_PANEL_SQS_ORDER_CHARGE_ORDER_RECEIPT=https://sqs.eu-west-1.amazonaws.com/569128858329/sqs-order-charge-order-receipt-dev-lejonu
 export SAMBERA_PANEL_SQS_PANELLIST_TO_TERMINATE=https://sqs.eu-west-1.amazonaws.com/569128858329/sqs-panellist-to-terminate-dev-lejonu
 export SAMBERA_PANEL_SQS_IPMS_PANELLIST_PANEL_MEMBERSHIPS=https://sqs.eu-west-1.amazonaws.com/569128858329/sqs-ipms-panellist-panel-memberships-dev-lejonu
